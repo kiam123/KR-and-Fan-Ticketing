0a92793012b1fffa41ca83ed9cfb9d5ac7dbd152
WuIFan <denny91002@gmail.com>
2019-06-10 22:54:14 +0800

    Feature : use firestore

--- app/src/main/java/ncku/edu/tw/kr_and_fan_ticketing/Fragment/HomeFragment.java
+++ app/src/main/java/ncku/edu/tw/kr_and_fan_ticketing/Fragment/HomeFragment.java
@@ -3,11 +3,21 @@ package ncku.edu.tw.kr_and_fan_ticketing.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import ncku.edu.tw.kr_and_fan_ticketing.Activity.SearchActivity;
 import ncku.edu.tw.kr_and_fan_ticketing.R;
@@ -37,8 +47,23 @@ public class HomeFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), SearchActivity.class);
-                getActivity().startActivity(intent);
+//                Intent intent = new Intent(getActivity(), SearchActivity.class);
+//                getActivity().startActivity(intent);
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("/searchResult/TPEKUL2019-07-10/tickets")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(document.getId() , document.get("flyTime").toString());
+                                    }
+                                } else {
+                                }
+                            }
+                        });
+
             }
         });
     }
