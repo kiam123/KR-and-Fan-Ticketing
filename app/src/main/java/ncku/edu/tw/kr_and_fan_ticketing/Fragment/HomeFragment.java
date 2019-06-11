98c5a63b5f58935092c57b3bdfcb69bfe9d481e3
WuIFan <denny91002@gmail.com>
2019-06-11 16:04:16 +0800

    Feature : show search result

--- app/src/main/java/ncku/edu/tw/kr_and_fan_ticketing/Fragment/HomeFragment.java
+++ app/src/main/java/ncku/edu/tw/kr_and_fan_ticketing/Fragment/HomeFragment.java
@@ -12,19 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import ncku.edu.tw.kr_and_fan_ticketing.Activity.SearchActivity;
 import ncku.edu.tw.kr_and_fan_ticketing.R;
 
@@ -33,6 +20,7 @@ import ncku.edu.tw.kr_and_fan_ticketing.R;
  */
 public class HomeFragment extends Fragment {
 
+    Intent intent;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -53,43 +41,22 @@ public class HomeFragment extends Fragment {
         final EditText edit_ori = viewGroup.findViewById(R.id.edit_ori);
         final EditText edit_dst = viewGroup.findViewById(R.id.edit_dst);
         final EditText edit_date = viewGroup.findViewById(R.id.edit_date);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(getActivity(), SearchActivity.class);
-//                getActivity().startActivity(intent);
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                Map<String,String> query = new HashMap<>();
-//                String date = "2019-06-30";
-//                String ori = "TPE";
-//                String dst = "GUM";
-                String date = edit_date.getText().toString();
-                String ori = edit_ori.getText().toString();
-                String dst = edit_dst.getText().toString();
-                query.put("date", date);
-                query.put("ori", ori);
-                query.put("dst", dst);
-                String user = "developer";
-                String path = "/user/" + user + "/query/";
-                String id = ori + dst + date;
-                String msg = "run : " + id;
-                Log.d("state",msg);
-                db.collection(path).document(id)
-                        .set(query)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d("db", "DocumentSnapshot successfully written!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d("db", "Error writing document");
-                            }
-                        });
+                intent = new Intent(getActivity(), SearchActivity.class);
+                intent.putExtra("date",view2String(edit_date));
+                intent.putExtra("ori",view2String(edit_ori));
+                intent.putExtra("dst",view2String(edit_dst));
+                getActivity().startActivity(intent);
 
+            }
 
+            public String view2String(EditText edt){
+                String v2S = edt.getText().toString();
+                return  v2S;
             }
         });
     }
